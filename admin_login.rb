65b9ae82f3172b2b7665d112c361f57c7ac817a3
ma wei <wma@thoughtworks.com>
2014-06-19 00:07:23 +0800

    add login check for admin center, and register token for second challenge

--- admin_login.rb
+++ admin_login.rb
@@ -7,6 +7,7 @@ post '/administrator/signin' do
   password = params[:password]
 
   if (username.eql?("admin") && password.eql?("1qaz!QAZ"))
+    session[:admin_auth_status] = true
     redirect '/administrator/center'
   else
     redirect '/administrator/signin'
@@ -14,10 +15,16 @@ post '/administrator/signin' do
 end
 
 get '/administrator/center' do
+  redirect '/administrator/signin' if session[:admin_auth_status].nil?
+
+  session[:token_2] = (generate_token CHALLENGE_2) if session[:token_2].nil?
+
   erb :admin_dashboard
 end
 
 get '/administrator/signout' do
   session[:admin_auth_status] = nil
+  session[:token_2] = nil
+
   redirect '/administrator/signin'
 end
